570143dae87268d0fba4114310c89c9b744f681d
QQ652276536 <652276536@qq.com>
2019-07-08 16:57:06 +0800

    提交项目

---
+++ src/main/java/com/zistone/controller/UserInfoController.java
@@ -0,0 +1,23 @@
+package com.zistone.controller;
+
+import com.zistone.entity.UserInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/UserInfo")
+public class UserInfoController {
+
+    Logger logger = LoggerFactory.getLogger(UserInfoController.class);
+
+    @RequestMapping(value = "/Register", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public String Register(@RequestBody UserInfo userInfo) {
+        logger.info("收到注册请求,参数是:"+userInfo.toString());
+
+        return "1";
+    }
+}
---
+++ src/main/java/com/zistone/entity/UserInfo.java
@@ -0,0 +1,135 @@
+package com.zistone.entity;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "userinfo")
+public class UserInfo {
+
+    public UserInfo() {
+    }
+
+    @Override
+    public String toString() {
+        return "UserInfo{" + "m_id=" + m_id + ", m_userName='" + m_userName + '\'' + ", m_realName='" + m_realName + '\'' + ", m_phoneNumber=" + m_phoneNumber + ", m_level=" + m_level + ", m_isDelete=" + m_isDelete + ", m_craeteTime=" + m_craeteTime + ", m_updateTime=" + m_updateTime + '}';
+    }
+
+    /**
+     * 用户编号(由数据库生成)
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int m_id;
+
+    public int getM_id() {
+        return m_id;
+    }
+
+    public void setM_id(int m_id) {
+        this.m_id = m_id;
+    }
+
+    /**
+     * 用户名
+     */
+    @Column(nullable = false, columnDefinition = "varchar(50) default '' comment '用户名'")
+    private String m_userName;
+
+    public String getM_userName() {
+        return m_userName;
+    }
+
+    public void setM_userName(String m_userName) {
+        this.m_userName = m_userName;
+    }
+
+    /**
+     * 用户实名
+     */
+    @Column(nullable = false, columnDefinition = "varchar(50) default '' comment '用户实名'")
+    private String m_realName;
+
+    public String getM_realName() {
+        return m_realName;
+    }
+
+    public void setM_realName(String m_realName) {
+        this.m_realName = m_realName;
+    }
+
+    /**
+     * 注册手机号
+     */
+    @Column(nullable = false, columnDefinition = "varchar(50) default '' comment '注册手机号'")
+    private int m_phoneNumber;
+
+    public int getM_phoneNumber() {
+        return m_phoneNumber;
+    }
+
+    public void setM_phoneNumber(int m_phoneNumber) {
+        this.m_phoneNumber = m_phoneNumber;
+    }
+
+    /**
+     * 用户权限
+     */
+    @Column(nullable = false, columnDefinition = "int default '1' comment '用户权限:1普通用户2管理员3超级管理员'")
+    private int m_level;
+
+    public int getM_level() {
+        return m_level;
+    }
+
+    public void setM_level(int m_level) {
+        this.m_level = m_level;
+    }
+
+    /**
+     * 用户留痕
+     */
+    @Column(columnDefinition = "int default '1' comment '用户留痕:0已注销1正在使用'")
+    private boolean m_isDelete;
+
+    public boolean isM_isDelete() {
+        return m_isDelete;
+    }
+
+    public void setM_isDelete(boolean m_isDelete) {
+        this.m_isDelete = m_isDelete;
+    }
+
+    /**
+     * 创建时间
+     */
+    @CreatedDate
+    @Column(columnDefinition = "datetime default CURRENT_TIMESTAMP comment '创建时间'")
+    private Date m_craeteTime;
+
+    public Date getM_craeteTime() {
+        return m_craeteTime;
+    }
+
+    public void setM_craeteTime(Date m_craeteTime) {
+        this.m_craeteTime = m_craeteTime;
+    }
+
+    /**
+     * 修改时间
+     */
+    @LastModifiedDate
+    @Column(columnDefinition = "datetime default CURRENT_TIMESTAMP comment '修改时间'")
+    private Date m_updateTime;
+
+    public Date getM_updateTime() {
+        return m_updateTime;
+    }
+
+    public void setM_updateTime(Date m_updateTime) {
+        this.m_updateTime = m_updateTime;
+    }
+}

e4846569e5f0b50acd83c817e6afd48cf951ff25
QQ652276536 <652276536@qq.com>
2020-05-19 09:20:29 +0800

    1、添加了文件下载 2、注释了163邮箱的服务

--- src/main/java/com/zistone/gprs/StartApplication.java
+++ src/main/java/com/zistone/gprs/StartApplication.java
@@ -3,7 +3,6 @@ package com.zistone.gprs;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import com.zistone.gprs.service.EmailTask_YX;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
@@ -12,7 +11,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 import java.nio.charset.Charset;
 import java.util.ArrayList;
@@ -34,18 +32,18 @@ public class StartApplication
     /**
      * 配置定时任务,用于读取163邮箱的铱星网关发来的邮件
      */
-    @Scheduled(fixedRate = 10 * 60 * 1000)
-    public void FixedRate_EmailService_YX()
-    {
-        try
-        {
-            new EmailTask_YX();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
+//    @Scheduled(fixedRate = 10 * 60 * 1000)
+//    public void FixedRate_EmailService_YX()
+//    {
+//        try
+//        {
+//            new EmailTask_YX();
+//        }
+//        catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+//    }
 
     /**
      * 配置fastjson消息转换器
---
+++ src/main/java/com/zistone/gprs/controller/DownloadController.java
@@ -0,0 +1,87 @@
+package com.zistone.gprs.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+//@RestController = @Controller + @ReponseBody
+@RestController
+@RequestMapping(value = "/Download")
+public class DownloadController {
+    private Logger _logger = LoggerFactory.getLogger(DownloadController.class);
+
+    @RequestMapping(value = "/OTATest", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    public ResponseEntity<Resource> OTATest(HttpServletRequest request) throws IOException {
+        _logger.info("收到下载更新包的请求");
+        //待下载文件名
+        String fileName = "update.zip";
+        File file = new File(fileName);
+
+
+        if (Objects.nonNull(file)) {
+            Resource resource = null;
+            String contentType = null;
+            try {
+                resource = new FileSystemResource(file);
+                contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+                _logger.info("已获取到要下载的更新包资源");
+            } catch (IOException e) {
+                _logger.error(e.getMessage());
+            }
+            if (Objects.isNull(contentType)) {
+                contentType = "application/octet-stream";
+            }
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                    .body(resource);
+        } else {
+            throw new IOException("资源未找到！");
+        }
+    }
+
+
+    @RequestMapping(value = "/TxtTest", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    public ResponseEntity<Resource> TxtTest(HttpServletRequest request) throws IOException {
+        _logger.info("收到下载更新文本的请求");
+        //待下载文件名
+        String fileName = "update_info.txt";
+        File file = new File(fileName);
+
+
+        if (Objects.nonNull(file)) {
+            Resource resource = null;
+            String contentType = null;
+            try {
+                resource = new FileSystemResource(file);
+                contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+                _logger.info("已获取到要下载的文本资源");
+            } catch (IOException e) {
+                _logger.error(e.getMessage());
+            }
+            if (Objects.isNull(contentType)) {
+                contentType = "text/plain";
+            }
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                    .body(resource);
+        } else {
+            throw new IOException("资源未找到！");
+        }
+    }
+
+}
